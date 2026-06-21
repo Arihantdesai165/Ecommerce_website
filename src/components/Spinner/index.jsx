f0256f917f537c5266ea37c1cd546b08025da7a3
Arihantdesai165 <arihantdesai483@gmail.com>
2026-06-21 22:54:01 +0530

    feat: add Spinner component

---
+++ src/components/Spinner/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Spinner = ({ size = 'md' }) => {
+  const sizeClasses = {
+    sm: 'w-4 h-4 border-2',
+    md: 'w-8 h-8 border-4',
+    lg: 'w-12 h-12 border-4',
+  };
+
+  return (
+    <div className={`rounded-full border-t-transparent border-blue-600 animate-spin ${sizeClasses[size]}`}></div>
+  );
+};
+
+export default Spinner;

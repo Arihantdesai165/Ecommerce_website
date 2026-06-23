1e51c3fadca3180c285182e3ee85f06681a254f5
Arihantdesai165 <arihantdesai483@gmail.com>
2026-06-23 15:11:20 +0530

    a11y: add role contentinfo to Footer

--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -4,7 +4,7 @@ import { FOOTER_INFO } from "~/constants";
 const Footer = () => {
   const { content } = FOOTER_INFO;
   return (
-    <footer className="bg-primary py-12">
+    <footer role="contentinfo" className="bg-primary py-12">
       <div className="container mx-auto">
         <p className="text-white text-center">{content}</p>
       </div>

b4ad80c871d5c0f9c376d917e6bfe426ca70236a
Arihantdesai165 <arihantdesai483@gmail.com>
2026-06-26 18:52:39 +0530

    refactor: add PropTypes validation to Card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+/**
+ * Card component for displaying content in a structured box.
+ * @param {Object} props - Component props.
+ * @param {React.ReactNode} props.children - Card content.
+ * @param {string} props.className - Additional CSS classes.
+ */
 const Card = ({ children, className = '' }) => {
   return (
     <div className={`bg-white shadow-md rounded-lg p-4 border border-gray-200 ${className}`}>
       {children}
     </div>
   );
+};
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
 
 export default Card;

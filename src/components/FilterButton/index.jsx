ebaa93f20a4f35756b2c6f3e3f02f0c4e0546847
Arihantdesai165 <arihantdesai483@gmail.com>
2026-06-26 18:53:07 +0530

    refactor: add PropTypes validation to FilterButton component

--- src/components/FilterButton/index.jsx
+++ src/components/FilterButton/index.jsx
@@ -1,5 +1,12 @@
 import "./styles.css";
 
+/**
+ * FilterButton component for category selection.
+ * @param {Object} props - Component props.
+ * @param {string} props.content - Button text/category name.
+ * @param {Function} props.onClick - Click handler.
+ * @param {string} props.selectedCategory - Currently selected category.
+ */
 const FilterButton = ({ content, onClick, selectedCategory }) => {
   return (
     <button
@@ -22,6 +29,14 @@ const FilterButton = ({ content, onClick, selectedCategory }) => {
     </button>
   );
 };
+
+import PropTypes from 'prop-types';
+FilterButton.propTypes = {
+  content: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  selectedCategory: PropTypes.string,
+};
+
 export default FilterButton;
 
 /* minor update 11 */

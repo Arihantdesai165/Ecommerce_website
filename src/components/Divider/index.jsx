de4e62f381286093cff3d87d9aa3c2916c311c17
Arihantdesai165 <arihantdesai483@gmail.com>
2026-06-26 18:52:58 +0530

    refactor: add PropTypes validation to Divider component

--- src/components/Divider/index.jsx
+++ src/components/Divider/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+/**
+ * Divider component to separate content visually.
+ * @param {Object} props - Component props.
+ * @param {string} props.className - Additional CSS classes.
+ */
 const Divider = ({ className = '' }) => {
   return (
     <hr className={`border-t border-gray-200 my-4 ${className}`} />
   );
+};
+
+Divider.propTypes = {
+  className: PropTypes.string,
 };
 
 export default Divider;

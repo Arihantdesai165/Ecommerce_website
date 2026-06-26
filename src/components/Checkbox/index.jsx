8552eb3380e06766ebba3ff4cee5afac1f1c4d87
Arihantdesai165 <arihantdesai483@gmail.com>
2026-06-26 18:52:52 +0530

    refactor: add PropTypes validation to Checkbox component

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+/**
+ * Checkbox component for boolean user input.
+ * @param {Object} props - Component props.
+ * @param {string} props.label - Checkbox label text.
+ * @param {boolean} props.checked - Checked state.
+ * @param {Function} props.onChange - Change handler.
+ * @param {boolean} props.disabled - Disabled state.
+ */
 const Checkbox = ({ label, checked, onChange, disabled = false }) => {
   return (
     <label className="flex items-center space-x-2 cursor-pointer">
@@ -13,6 +22,13 @@ const Checkbox = ({ label, checked, onChange, disabled = false }) => {
       {label && <span className={`text-gray-700 ${disabled ? 'opacity-50' : ''}`}>{label}</span>}
     </label>
   );
+};
+
+Checkbox.propTypes = {
+  label: PropTypes.string,
+  checked: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
 };
 
 export default Checkbox;
